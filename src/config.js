a62e89f3a25c1b3532f119533d2b0995736eebcc
eliel <eliel.niukkanen@medisapiens.fi>
2020-08-04 19:17:18 +0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "eliels-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://9p06bft75d.execute-api.eu-central-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_nrP5QgCmS",
+    APP_CLIENT_ID: "5eiajs17t1e7p970lhg6m8gr9p",
+    IDENTITY_POOL_ID: "eu-central-1:23cbca6d-0e51-4972-93d4-fa51e60ac2b1"
+  }
+};
